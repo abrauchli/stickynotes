e1651261a6bbd4e69eee88c55503ff814126cbe4
Andreas Brauchli <a.brauchli@elementarea.net>
2012-08-28 00:49:56 +0200

    Drag-n-Drop start

--- extension.js
+++ extension.js
@@ -35,26 +35,28 @@ const StickyNote = new Lang.Class({
 
 	_init: function() {
 		this.locked = false;
-		this.geometry = { x: 0, y: 0, w: 50, h: 50 };
+		this.position = null;
+		this.size = {width: 100, height: 100};
 		this.title = null;
 		this.content = '';
 		this.font = null;
 		this.fontColor = null;
 		this.color = null;
 
-		let layoutManager = new Clutter.BinLayout({
-			x_align: Clutter.BinAlignment.FIXED,
-			y_align: Clutter.BinAlignment.FIXED
+		this.widget = new Clutter.Actor({
+			opacity: 0,
+			reactive: true,
+			width: this.size.width,
+			height: this.size.height
 		});
-		this.widget = new Clutter.Actor();
-		this.widget.set_layout_manager(layoutManager);
-		this.widget.set_opacity(0);
+		if (this.position) {
+			this.widget.position = position;
+		}
 		this._noteFrameActor = new St.Bin({ style_class: 'note' });
 
 		this._noteActor = new St.Entry({
 			text: "Foo note\nBar line",
-			style_class: 'note-text',
-			track_hover: true
+			style_class: 'note-text'
 		});
 		this._noteActor.get_clutter_text().set_single_line_mode(false);
 
@@ -63,20 +65,55 @@ const StickyNote = new Lang.Class({
 			opacity: 20,
 			margin_top: 5.0,
 			margin_right: 5.0,
-			x_expand: true,
-			x_align: St.Align.END
+			//x_expand: true,
+			x_align: St.Align.END,
+			z_position: 1
 		});
 		this._btnClose.connect('clicked', Lang.bind(this, this.destroy));
-		this._noteActor.connect('notify::hover', Lang.bind(this, this._tweenCloseButton));
 
 		this._noteFrameActor.add_actor(this._noteActor);
 		this.widget.add_actor(this._noteFrameActor);
 		this.widget.add_actor(this._btnClose);
 
+		// Moving the note
+
+		// This doesn't work unfortunately
+		// let dragAction = new Clutter.DragAction();
+		// this.widget.add_action(dragAction);
+
+		this.widget.connect('button-press-event', Lang.bind(this, this._startDrag));
+		this.widget.connect('button-release-event', Lang.bind(this, this._endDrag));
+		this.widget.connect('enter-event', Lang.bind(this, this._tweenCloseButton));
+		this.widget.connect('leave-event', Lang.bind(this, this._tweenCloseButton));
+
 	},
-	_tweenCloseButton: function() {
+	_startDrag: function(evt) {
+		log('start '+evt.x+' '+evt.y);
+		this._drag_orig = {x: evt.x, y: evt.y };
+		this._handlerId = this.widget.connect('motion-event', Lang.bind(this, this._drag));
+		return true;
+	},
+	_drag: function(evt) {
+		log('drag '+evt.x+' '+evt.y);
+		if (!this._drag_orig) {
+			return false;
+		}
+		this.widget.move_by(evt.x - this._drag_orig.x, evt.y - this._drag_orig.y);
+		return true;
+	},
+	_endDrag: function(evt) {
+		log('end '+evt.x+' '+evt.y);
+		if (!this._drag_orig) {
+			return false;
+		}
+		this.widget.disconnect(this._handlerId);
+		delete this._handlerId;
+		delete this._drag_orig;
+		return true;
+	},
+	_tweenCloseButton: function(act, evt, data) {
 		Tweener.addTween(this._btnClose,
-						 { opacity: (this._noteActor.hover ? 200 : 20),
+						 { opacity: (evt.type() === Clutter.EventType.ENTER ? 200 : 20),
 						   time: 0.1,
 						   transition: 'easeOutQuad' });
 	},
@@ -107,8 +144,26 @@ const StickyNotesManager = new Lang.Class({
 	Name: 'StickyNotesManager',
 
 	_init: function(stickyNotesPaneActor) {
-		this.paneActor = new St.Bin();
+		let layoutManager = new Clutter.BinLayout({
+			x_align: Clutter.BinAlignment.FIXED,
+			y_align: Clutter.BinAlignment.FIXED
+		});
+		this.paneActor = new Clutter.Actor({
+			layout_manager: layoutManager,
+			reactive: true
+		});
+
 		this.notes = [];
+		this.pos_x = 20;
+
+		let btnAdd = new St.Button({
+			label: '+',
+			z_position: 1,
+			x: 10,
+			y: 10
+		});
+		btnAdd.connect('clicked', Lang.bind(this, this.createNote));
+		this.paneActor.add_actor(btnAdd);
 
 		this.createNote();
 	},
@@ -126,6 +181,8 @@ const StickyNotesManager = new Lang.Class({
 	},
 	createNote: function() {
 		let note = new StickyNote();
+		note.setPosition(this.pos_x, 0);
+		this.pos_x += 120;
 		this.notes.push(note);
 		this.addNote(note);
 	}
